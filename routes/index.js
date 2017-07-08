9cd0500ecb97a2ee1d755aee3a61a83818d104f7
speed351 <zpzp351@naver.com>
2017-07-08 11:53:15 +0900

    init heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("test2");
+  var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '111111',
+    port     : 3306,
+    database : 'weathers'
+  });
+
+  connection.connect();
+
+  connection.query('SELECT * from life_info', function(err, rows, fields) {
+    if (!err){
+      console.log('호출받음 The solution is: ', rows);
+      console.log(req.body);
+
+      res.send(rows);
+
+    }
+    else{
+      console.log('Error while performing Query.', err);
+      res.send(err);
+
+    }
+  });
+
+  connection.end();
 });
+
 
 module.exports = router;
